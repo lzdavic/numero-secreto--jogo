8005463bc81fa96f8100676223f42028d6a49568
Lzdavic <lzdavicc@gmail.com>
2023-01-09 16:46:59 -0300

    Atualizacoes

--- app/validacao.js
+++ app/validacao.js
@@ -2,7 +2,18 @@ function verificaChuteValido() {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-       elementoChute.innerHTML +='<div>Chute invalido</div>';
+        if (chute.includes("desisto") || chute.includes("desiste")) {
+
+            document.body.innerHTML =
+                `
+                <h2>Fim do Jogo!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+            document.body.style.backgroundColor = "black";
+        } else {
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
     }
 
     if (numeroMaiorOuMenor(numero)) {
@@ -14,9 +25,18 @@ function verificaChuteValido() {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
-
 }
 
 function chuteForInvalido(numero) {
@@ -26,3 +46,9 @@ function chuteForInvalido(numero) {
 function numeroMaiorOuMenor(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
