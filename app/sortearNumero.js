4c6bc5175d926bccb71eab01ef4f17f8e8fb2740
Lzdavic <lzdavicc@gmail.com>
2023-01-12 21:38:12 -0300

    adicionar campos para inserir valores

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,5 +1,17 @@
-const menorValor = 1
-const maiorValor = 1000
+function getMenorValor() {
+    var menorValorMemory = sessionStorage.menor
+
+    if (sessionStorage.menor == "undefined") {
+        menorValor === 1;
+        return
+    } else {
+        menorValor = menorValorMemory + 1 - 0
+    }
+    return
+}
+
+var menorValor = parseInt(getMenorValor());
+var maiorValor = 1000
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
@@ -13,3 +25,47 @@ elementoMenorValor.innerHTML = menorValor
 
 const elementoMaiorValor = document.getElementById('maior-valor')
 elementoMaiorValor.innerHTML = maiorValor
+
+
+// alterar intervalo do sorteio
+
+const botaoAlterar = document.getElementById('alterar-intervalo');
+const inputMenor = document.getElementById('input-menor');
+const inputMaior = document.getElementById('input-maior');
+const botaoAdicionar = document.getElementById('input-button');
+
+botaoAlterar.addEventListener('click', e => {
+        mostrarInput();
+    })
+    
+    function mostrarInput() {
+        elementoMenorValor.innerHTML = `
+        <input type="number" id="input-menor">
+        `
+        elementoMaiorValor.innerHTML = `
+        <input type="number" id="input-maior">
+        `
+        botaoAdicionar.dataset.display = "show"
+    }
+    
+    botaoAdicionar.addEventListener('click', () => {
+        salvaIntervalo();
+    })
+    
+    
+    function salvaIntervalo() {
+    var inputMenor = document.getElementById("input-menor").value
+    var inputMaior = document.getElementById("input-maior").value
+    
+
+        if (inputMenor >= inputMaior || 
+            inputMenor <= 0 || 
+            inputMaior <= 0) {
+                window.alert("O valor menor não pode ultrapassar o valor maior e os valores NÃO podem ser negativos")
+        } else {
+            sessionStorage.setItem("menor", inputMenor)
+            sessionStorage.setItem("maior", inputMaior)
+        }
+}
+
+
